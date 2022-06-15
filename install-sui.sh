f228ec04cc6d5326d6251738ad27c65aaa952148
agonyp <beenejer@gmail.com>
2022-06-15 15:04:31 +0300

    Sei update

--- install-sui.sh
+++ install-sui.sh
@@ -32,7 +32,7 @@ if [ ! $NODENAME ]; then
 	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
 fi
 echo "export WALLET=wallet" >> $HOME/.bash_profile
-echo "export CHAIN_ID=sei-testnet2" >> $HOME/.bash_profile
+echo "export CHAIN_ID=sei-testnet-2" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 echo '================================================='
