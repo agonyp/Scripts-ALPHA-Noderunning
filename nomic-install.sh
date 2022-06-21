f460c383eefc171fb7d198d183144028da25982a
agonyp <beenejer@gmail.com>
2022-06-21 20:57:00 +0300

    Upd

--- nomic-install.sh
+++ nomic-install.sh
@@ -43,7 +43,7 @@ git clone https://github.com/nomic-io/nomic.git nomic && cd nomic
 git checkout develop
 
 # build and install, adding a `nomic` command to your PATH
-cargo build -p nomic:2.0.1 --release || return
+nomic-build () { cargo build -p nomic:2.0.1 --release;}
 
 sudo mv ~/nomic/target/release/nomic /usr/local/bin/
 
