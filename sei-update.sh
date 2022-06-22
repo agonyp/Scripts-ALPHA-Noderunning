9d7a610aa223be8ad46fee2726a9a589f0309740
agonyp <beenejer@gmail.com>
2022-06-23 00:50:25 +0300

    Upd

--- sei-update.sh
+++ sei-update.sh
@@ -2,11 +2,8 @@
 git clone https://github.com/sei-protocol/sei-chain.git
 cd sei-chain/
 git fetch --tags -f
-git checkout 1.0.3beta
+git checkout 1.0.4
 
 make install
 
 sudo systemctl restart seid
-
-export CHAIN_ID=sei-testnet-2
-source $HOME/.bash_profile
